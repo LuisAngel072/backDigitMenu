a2fc5b926bf9ed3189ee4f816ce3c4c2ed71268a
luisangel072 <luis.delatorre5745@alumnos.udg.mx>
2025-05-05 08:35:38 -0600

    fix(pedidos) -> Falta de importacion de entidades en el modulo arreglado

--- src/pedidos/pedidos.module.ts
+++ src/pedidos/pedidos.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { PedidosController } from './pedidos.controller';
 import { PedidosService } from './pedidos.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pedidos } from './entities/pedidos.entity';
+import { Pedidos_has_extrassel } from './entities/pedidos_has_extrasSel.entity';
+import { Pedidos_has_ingrsel } from './entities/pedidos_has_ingrSel.entity';
+import { Productos } from 'src/productos/entities/productos.entity';
+import { Mesas } from 'src/mesas/entities/mesa.entity';
+import { Opciones } from 'src/opciones/entities/opciones.entity';
+import { Ingredientes } from 'src/ingredientes/entities/ingredientes.entity';
+import { Extras } from 'src/extras/entities/extras.entity';
 
 @Module({
   controllers: [PedidosController],
-  providers: [PedidosService]
+  providers: [PedidosService],
+  imports: [
+    TypeOrmModule.forFeature([
+      Pedidos,
+      Pedidos_has_extrassel,
+      Pedidos_has_ingrsel,
+      Productos,
+      Mesas,
+      Opciones,
+      Extras,
+      Ingredientes,
+    ]),
+  ],
 })
 export class PedidosModule {}
