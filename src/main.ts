71a8f6e3b0cc3ad1811c47679558d8704e977e4f
VictorRTXD <victor.macias5736@alumnos.udg.mx>
2025-11-26 17:34:37 -0600

    fix(main): arreglando cors

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200', // Origen Angular
+    origin: '*', // Origen Angular
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
